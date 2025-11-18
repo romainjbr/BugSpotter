c6b2267ae6a1ac5c696106176d636689e27f1bf9
romainjbr <romainjbrafai@gmail.com>
2025-11-18 16:21:48 +0100

    Implement first tests of BugServiceTests

--- test/Core.Tests/Services/BugServiceTest.cs
+++ test/Core.Tests/Services/BugServiceTest.cs
@@ -1,10 +1,17 @@
 using Core.Entities;
 using Core.Dtos.Bugs;
+using Core.Interfaces;
+using Core.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
 
 namespace Core.Tests.Services.BugServiceTest;
 
 public class BugServiceTests
 {
+    readonly Mock<ILogger<BugService>> _logger;
+    readonly Mock<IRepository<Bug>> _repo;
+    readonly BugService _svc;
     private static Bug MakeBug() => new()
     {
         Id = Guid.NewGuid(),
@@ -16,4 +23,71 @@ public class BugServiceTests
     private static BugCreateDto MakeCreateDto() => new("Ant", 1, "Tiny");
 
     private static BugUpdateDto MakeUpdateDto(Guid id) => new(id, "Bee", 2, "Buzz");
+
+    public BugServiceTests()
+    {
+
+        _logger = new Mock<ILogger<BugService>>();
+        _repo = new Mock<IRepository<Bug>>();
+        _svc = new BugService(_logger.Object, _repo.Object);        
+    }
+
+#region AddAsync 
+
+    [Fact]
+    public async Task AddAsync_CallsRepo_And_ReturnsDto()
+    {
+        var dto = MakeCreateDto();
+
+        _repo.Setup(x => x.AddAsync(It.IsAny<Bug>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        var result = await _svc.AddAsync(dto, CancellationToken.None);
+
+        _repo.Verify(x => x.AddAsync(
+            It.Is<Bug>(b =>
+                b.Species == "Ant" &&
+                b.DangerLevel == 1 &&
+                b.Description == "Tiny"),
+            It.IsAny<CancellationToken>()),
+            Times.Once);
+
+        Assert.Equal("Ant", result.Species);
+        Assert.Equal(1, result.DangerLevel);
+        Assert.Equal("Tiny", result.Description);
+    }
+
+#endregion
+
+#region GetByIdAsync
+
+    [Fact]
+    public async Task GetByIdAsync_IdNotFound_ReturnsNull()
+    {
+        _repo.Setup(x => x.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Bug?)null);
+
+        var result = await _svc.GetByIdAsync(Guid.NewGuid(), CancellationToken.None);
+
+        Assert.Null(result);
+    }
+
+#endregion
+
+#region ListAsync
+
+    [Fact]
+    public async Task ListAsync_ReturnsListOfBugs()
+    {
+        var list = new List<Bug> { MakeBug(), MakeBug() };
+
+        _repo.Setup(x => x.ListAsync(It.IsAny<CancellationToken>())).ReturnsAsync(list);
+
+        var allBugs = await _svc.ListAsync(CancellationToken.None);
+
+        Assert.Equal(2, allBugs.Count);
+        Assert.True(allBugs.All(x => list.Any(b => b.Id == x.Id)));
+    }
+
+#endregion
 }
