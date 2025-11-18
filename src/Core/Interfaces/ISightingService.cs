c1a3956bb05b303735097c69743534ad438f0f81
romainjbr <romainjbrafai@gmail.com>
2025-11-18 17:19:14 +0100

    Add ISightingService interface

---
+++ src/Core/Interfaces/ISightingService.cs
@@ -0,0 +1,13 @@
+using Core.Dtos.Sightings;
+using Core.Entities;
+
+namespace Core.Interfaces; 
+
+public interface ISightingService
+{
+    Task<SightingReadDto?> GetByIdAsync(Guid id, CancellationToken token);
+    Task<List<SightingReadDto>> ListAsync(CancellationToken token);
+    Task<SightingReadDto?> AddAsync(SightingCreateDto dto, CancellationToken token);
+    Task<bool> UpdateAsync(SightingUpdateDto dto, CancellationToken token);
+    Task<bool> DeleteAsync(Guid id, CancellationToken token);
+}
