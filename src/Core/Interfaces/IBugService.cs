717cc94e0afbffa5f068f720d5c5fcb67af1802e
romainjbr <romainjbrafai@gmail.com>
2025-11-18 14:49:24 +0100

    Add IBugService interface

---
+++ src/Core/Interfaces/IBugService.cs
@@ -0,0 +1,12 @@
+using Core.Dtos.Bugs;
+
+namespace Core.Interfaces; 
+
+public interface IBugService
+{
+    Task<BugReadDto?> GetByIdAsync(Guid id, CancellationToken token);
+    Task<List<BugReadDto>> ListAsync(CancellationToken token);
+    Task<BugReadDto> AddAsync(BugCreateDto dto, CancellationToken token);
+    Task<bool> UpdateAsync(BugUpdateDto dto, CancellationToken token);
+    Task<bool> DeleteAsync(Guid id, CancellationToken token);
+}
