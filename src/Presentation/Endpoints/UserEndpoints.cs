ea824d17e1e61830d79cbb2b39a692f237c07e55
romainjbr <romainjbrafai@gmail.com>
2025-11-21 09:09:50 +0100

    Implement user endpoints for /login, /register and /all

--- src/Presentation/Endpoints/UserEndpoints.cs
+++ src/Presentation/Endpoints/UserEndpoints.cs
@@ -8,6 +8,40 @@ public static class UserEndpoints
 {
     public static IEndpointRouteBuilder MapUserEndpoints(this IEndpointRouteBuilder routes)
     {
-        return routes;
+        var group = routes.MapGroup("/user");
+
+        group.MapPost("/register", async Task<Results<Ok<string>, Conflict<string>>> (IUserService svc, UserCreateDto dto, CancellationToken token) =>
+        {
+            var receivedToken = await svc.RegisterAsync(dto, token);
+
+            if (string.IsNullOrWhiteSpace(receivedToken))
+            {
+                return TypedResults.Conflict("User already exists");
+            }
+
+            return TypedResults.Ok(receivedToken);
+        });
+
+        group.MapPost("/login", async Task<Results<Ok<string>, Conflict<string>>> (IUserService svc, UserLoginDto dto, CancellationToken token) =>
+        {
+            var receivedToken = await svc.LoginAsync(dto, token);
+
+            if (string.IsNullOrWhiteSpace(receivedToken))
+            {
+                return TypedResults.Conflict("Email or password is incorrect");
+            }
+
+            return TypedResults.Ok(receivedToken);
+        });
+
+        // TODO: Implemet Authorization/Authentication and make this method accessible for authorized users only
+        group.MapGet("/all", async Task<Results<Ok<List<UserReadDto>>, UnauthorizedHttpResult>> (IUserService svc, CancellationToken token) =>
+        {
+            var allUsers = await svc.ListAsync(token);
+
+            return TypedResults.Ok(allUsers);
+        });
+        
+       return routes;
     }
 }
